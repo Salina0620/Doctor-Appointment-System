1742f1268190ca59fe62651f110f9ca5e5edc577
salina <Salina Adhikari>
2024-08-22 16:14:17 +0545

    doctor appointment

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Appointment;
+use App\Models\Patient;
+use App\Models\Doctor;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $total_doctor = Doctor::count();
+        $total_patient = Patient::count();
+        $total_appointment = Appointment::count();
+
+        return view('admin.dashboard', compact('total_doctor', 'total_patient', 'total_appointment'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
